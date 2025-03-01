22f457e9ffeb2e4b669b60b0ab384c710b34cd56
Ocean Fuaga <oceansfuaga@gmail.com>
2025-02-28 20:24:44 -0600

    Set up landing page

---
+++ components/CTAButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="relative">
+    <button
+      class="bg-orange-500 hover:bg-orange-600 text-white font-bold mt-4 py-3 px-6 rounded-xl border-4 border-white transition duration-300 shadow-md flex items-center justify-center gap-2"
+    >
+      <span class="md:text-2xl"> NOTIFY ME </span>
+    </button>
+    <div class="absolute -z-10 left-2 top-6 w-[183px] h-[64px] bg-orange-600 rounded-2xl"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+
+<style scoped>
+button {
+  font-family: Istok Web, sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-[url(/img/hero-bg.png)] h-screen bg-fixed bg-cover bg-no-repeat">
+    <div class="sticky z-20 md:top-6 top-3 md:left-12 left-6 lg:w-40 w-28">
+      <a href="#" class="hover:border-2">
+        <img src="/img/logo.png" alt="OG Studios Logo" />
+      </a>
+    </div>
+    <div
+      class="flex flex-col items-center gap-4 absolute z-10 top-[170px] md:left-[200px] md:w-1/2"
+    >
+      <img src="/img/duckducklogowithduck.png" alt="Duck duck logo" />
+      <h1 class="lg:text-6xl md:text-4xl text-2xl font-bold text-white">COMING SOON TO STEAM</h1>
+      <CTAButton />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+
+<style scoped>
+* {
+  cursor: url("/img/duckcursor.png"), auto;
+}
+
+h1 {
+  font-family: Istok Web, sans-serif;
+}
+</style>
